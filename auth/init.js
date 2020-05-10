8de77a3680e2e179797c1a57893df28b763e04f3
Lucio Afonso <lucioafonso@icloud.com>
2020-05-10 18:32:15 +0200

    Feature: Auth SHA-1

--- auth/init.js
+++ auth/init.js
@@ -8,9 +8,14 @@ passport.serializeUser(function(user, done){
 });
 
 passport.deserializeUser(function(idUser, done){
-    db.query(`SELECT idUser, fullName, email, admin, active FROM users WHERE idUser="${idUser}";`, function (err, result){
-        done(err, result[0]);
-    });
+    db.query(`SELECT idUser, fullName, email, admin, active
+                FROM users
+                WHERE idUser="${idUser}";`,
+
+        function (err, result){
+            return done(err, result[0]);
+        }
+    );
 });
 
 function initPassport() {
@@ -21,32 +26,26 @@ function initPassport() {
         },
 
         function (req, username, password, done) {
-            const sql = `
-                SELECT * FROM users 
-                    WHERE email="${req.body.email}" 
-                        AND pass="${req.body.password}" 
-                        AND active>0;`;
+            db.query(`SELECT * FROM users
+                        WHERE email="${req.body.email}"
+                            AND pass="${req.body.password}"
+                            AND active>0;`,
 
-            db.query(sql, function (err, result) {
+                function (err, result) {
+                    if (err)
+                        return done(null, false, {message:err});
 
-                if (err)
-                    return done(null, false, {message:err});
+                    if (!result.length)
+                        return done(null, false);
 
-                if (!result.length)
-                    return done(null, false);
+                    let dbPassword = result[0].pass;
 
+                    if (!(req.body.password === dbPassword))
+                        return done(null, false);
 
-                let encPassword = req.body.password;
-                let dbPassword = result[0].pass;
-
-                if (!(encPassword === dbPassword))
-                    return done(null, false);
-
-
-                //console.log(result[0]);
-                console.log('n: '+result[0].fullName +' e: '+ result[0].email);
-                return done(null, result[0]);
-            });
+                    return done(null, result[0]);
+                }
+            );
         }
     ));
 
--- routes/login.js
+++ routes/login.js
@@ -3,19 +3,11 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function (req, res, next) {
-    let err = req.query.err;
-
     if(req.user)
         res.redirect('/library');
 
-    else {
-        if(err === "yes")
-            res.render('login', {err:err, user:req.user});
-        else
-            res.render('login', {err:null, user:req.user});
-    }
+    res.render('login', {err:req.query.err, user:req.user});
 });
-
 
 router.post('/', passport.authenticate('local', {
     successRedirect: '/library',
